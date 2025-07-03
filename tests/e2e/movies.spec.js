536483bf1bd77da7e2eac59314ea77e20dc0b4ec
PedroMendes414 <pedrooliverio4@gmail.com>
2025-07-03 20:33:03 -0300

    first commit

---
+++ tests/e2e/movies.spec.js
@@ -0,0 +1,19 @@
+const {test} = require ('../support')
+
+const data = require('../support/fixtures/movies.json')
+
+const { executeSQL } = require('../support/database')
+
+test('deve poder cadastrar um novo filme', async ({ page }) => {
+
+    //é importante estar logado
+    const movie = data.create
+    await executeSQL(`DELETE from movies WHERE title = '${movie.title}';`)
+
+    await page.login.visit()
+    await page.login.submit('admin@zombieplus.com', 'pwd123')
+    await page.movies.isLoggedIn()
+
+    await page.movies.create(movie)
+    await page.toast.containText('Cadastro realizado com sucesso!')
+})
